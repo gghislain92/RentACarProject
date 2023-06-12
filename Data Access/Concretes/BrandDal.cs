1a2b4366a581f7133327d8e506bd5e4ad20568f1
ShadyBenjy <shadbenjamim@gmail.com>
2023-06-13 00:34:33 +0300

    dal Brand Addition Commit

---
+++ Data Access/Concretes/BrandDal.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access.Concretes
+{
+    public class BrandDal{
+        public List<Brand> GetAll(){
+            List<Brand> brands = new List<Brand>();
+            brands.Add(new Brand { Id = 1, Name = "Nissan" });
+            brands.Add(new Brand { Id = 2, Name = "Toyota" });
+            brands.Add(new Brand { Id = 3, Name = "Mercedes Benz" });
+            brands.Add(new Brand { Id = 4, Name = "Audi" });
+            brands.Add(new Brand { Id = 5, Name = "Ford" });
+
+            return brands;
+        }
+    }
+}
