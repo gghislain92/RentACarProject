5fb689bd882ffc0bcdb6512b2f2e30eca8e90bd0
blazeeebit <blazeeebit@gmail.com>
2023-06-14 04:02:06 +0300

    delete model

--- Business/Dtos/Request/DeleteModelRequest.cs
+++ Business/Dtos/Request/DeleteModelRequest.cs
@@ -9,19 +9,6 @@ namespace Business.Dtos.Request
     public class DeleteModelRequest
     {
         public string Name { get; set; }
-    }
-}
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Business.Dtos.Request
-{
-    public class DeleteModelRequest
-    {
-        public string Name { get; set; }
         public double dailyPrice { get; set; }
 
         public int BrandId { get; set; }
